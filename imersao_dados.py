ff329203fa06f136d3e0082c17dabc91482d5676
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-08 13:56:26 -0300

    feat -  aula03

--- imersao_dados.py
+++ imersao_dados.py
@@ -142,3 +142,80 @@ print(df_limpo.info())
 print("\n")
 
 # AULA03
+import seaborn as sns
+import matplotlib.pyplot as plt
+# df_limpo['senioridade'].value_counts().plot(kind='bar', title="Distribuição de senioridade")
+
+'''Exemplo de gráfico 1'''
+# ordem = df_limpo.groupby('senioridade')['usd'].mean().sort_values(ascending=False).index
+# plt.figure(figsize=(8,5))
+# sns.barplot(data= df_limpo, x='senioridade', y ='usd', order=ordem)
+# plt.title('Salário médio por senioridade')
+# plt.xlabel("Senioridade")
+# plt.xlabel("Salário Médio Anual (USD)")
+# plt.show()
+
+'''Exemplo de gráfico 2'''
+# plt.figure(figsize=(10,4))
+# sns.histplot(df_limpo['usd'], bins = 50, kde=True)
+# plt.title('Distruibuição dos salários anuais')
+# plt.xlabel("Salário")
+# plt.ylabel("Frequência")
+# plt.show()
+
+'''Exemplo de gráfico 3'''
+# plt.figure(figsize=(8,5))
+# sns.boxplot(x=df_limpo['usd'])
+# plt.title("Boxplot Salário")
+# plt.xlabel("Salário em USD")
+# plt.show()
+
+'''Exemplo de gráfico 4'''
+# ordem_senioridade = ['junior', 'pleno','senior','executivo']
+# plt.figure(figsize=(8,5))
+# sns.boxplot(x='senioridade', y='usd', data=df_limpo, order=ordem_senioridade)
+# plt.title("Boxplot da distruibuição por senioridade")
+# plt.xlabel("Salário em USD")
+# plt.show()
+
+'''Exemplo de gráfico 5 colorido'''
+# ordem_senioridade = ['junior', 'pleno','senior','executivo']
+# plt.figure(figsize=(8,5))
+# sns.boxplot(x='senioridade', y='usd', data=df_limpo, order=ordem_senioridade, palette='Set2', hue='senioridade')
+# plt.title("Boxplot da distruibuição por senioridade")
+# plt.xlabel("Salário em USD")
+# plt.show()
+
+import plotly.express as px
+
+'''Exemplo de gráfico iterativo 1'''
+# senioridade_media_salario = df_limpo.groupby('senioridade')['usd'].mean().sort_values(ascending=False).reset_index()
+
+# fig = px.bar(senioridade_media_salario,
+#              x='senioridade',
+#              y='usd',
+#              title='Média Salarial por Senioridade',
+#              labels={'senioridade': 'Nível de Senioridade', 'usd': 'Média Salarial Anual (USD)'})
+# fig.show()
+
+'''Exemplo de gráfico iterativo 2'''
+# remoto_contagem = df_limpo['remoto'].value_counts().reset_index()
+# remoto_contagem.columns = ['tipo_trabalho','quantidade']
+
+# fig = px.pie(remoto_contagem,
+#              names='tipo_trabalho',
+#              values='quantidade',
+#              title='Proporção dos tipos de trabalho')
+# fig.show()
+
+'''Exemplo de gráfico iterativo 3'''
+# remoto_contagem = df_limpo['remoto'].value_counts().reset_index()
+# remoto_contagem.columns = ['tipo_trabalho','quantidade']
+
+# fig = px.pie(remoto_contagem,
+#              names='tipo_trabalho',
+#              values='quantidade',
+#              title='Proporção dos tipos de trabalho',
+#              hole= 0.5)
+# fig.update_traces(textinfo='percent+label')
+# fig.show()
